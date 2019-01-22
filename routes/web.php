22a3a9b2a6d045b5a790df76c6a17af682ba3b7a
Shane <sgray@sept24.com>
2019-01-22 13:04:49 -0500

    Remove welcome route

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
