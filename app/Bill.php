deb2a375c17b200c1328a32439950b6270d35be7
Shane <sgray@sept24.com>
2019-02-07 17:06:06 -0500

    Show total bill payments

--- app/Bill.php
+++ app/Bill.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Budget;
+use App\Purchases;
 use Illuminate\Database\Eloquent\Model;
 
 class Bill extends Model
@@ -14,6 +16,20 @@ class Bill extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get payments made to bill
+     * 
+     */
+    public function amount(Bill $bill, Budget $budget)
+    {
+        $payments = Purchase::where(['bill_id' => $bill->id, 'budget_id' => $budget->id])->get();
+        $amount = $payments->reduce(function($carry, $purchase) {
+            return $carry + $purchase->amount;
+        });
+
+        return number_format( (float) $amount, 2 );
     }
 
 }
--- resources/views/budgets/show.blade.php
+++ resources/views/budgets/show.blade.php
@@ -14,61 +14,7 @@
 
             @include('accounts.list')
 
-            <div class="card mb-4">
-
-                <div class="card-header">Bills</div>
-
-                <div class="card-body">
-
-                    @if( $bills->isNotEmpty() )
-
-                        <table class="table">
-
-                            <thead>
-
-                                <tr>
-
-                                    <th scope="col">ID</th>
-
-                                    <th scope="col">Name</th>
-
-                                    <th scrope="col">Amount</th>
-
-                                </tr>
-
-                            </thead>
-
-                            <tbody>
-
-                                @foreach( $bills as $bill )
-
-                                    <tr>
-
-                                        <th scope="row">{{ $bill->id }}</th>
-
-                                        <td>{{ $bill->name }}</td>
-
-                                        <td>${{ $bill->amount ?: '0.00' }}</td>
-
-                                    </tr>
-
-                                @endforeach
-
-                            </tbody>
-
-                        </table>
-
-                    @else
-
-                        <p>You have no bills</p>
-
-                    @endif
-
-                    <a href="/bills/create" class="btn btn-primary btn-primary float-right"><span class="oi oi-plus"></span> New bill</a>
-
-                </div>
-
-            </div>
+            @include('bills.list')
 
         </div>
 
